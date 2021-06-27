56c808983b9e28ee011c7412b7e41fd2e4954a94
Daniel Mateos <daniel@mateos.cc>
2021-06-27 22:05:24 +1000

    work on redis

--- jobmanager/tasks.py
+++ jobmanager/tasks.py
@@ -1,6 +1,15 @@
 from celery import Celery
+from l4mbda import settings
 
-app = Celery("tasks", backend="rpc://", broker="pyamqp")
+
+def build_app():
+    redis = "redis://{0}@{1}".format(
+        settings.REDIS["default"]["PASSWORD"], settings.REDIS["default"]["HOST"]
+    )
+    return Celery("tasks", backend=redis, broker=redis)
+
+
+app = build_app()
 
 
 @app.task
--- l4mbda/settings.py
+++ l4mbda/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from posix import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,6 +80,14 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+REDIS = {
+    "default": {
+        "HOST": os.environ.get("REDIS_HOST"),
+        "USER": os.environ.get("REDIS_USER"),
+        "PASSWORD": os.environ.get("REDIS_PASSWORD"),
     }
 }
 
