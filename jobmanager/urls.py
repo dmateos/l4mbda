87b1b15e68b7fcef09af121465035958ebb37d47
Daniel Mateos <daniel@mateos.cc>
2021-06-28 15:16:46 +1000

    templates and class views for job/jobruns

--- jobmanager/urls.py
+++ jobmanager/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.root), path("test", views.test)]
+urlpatterns = [
+    path("", views.root),
+    path("jobs", views.JobView.as_view()),
+    path("jobruns", views.JobRunView.as_view()),
+]
--- jobmanager/views.py
+++ jobmanager/views.py
@@ -1,10 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Job, JobRun
 
 
 def root(request):
     return render(request, "jobmanager/index.html", {})
 
 
-def test(request):
-    return HttpResponse("hello world")
+class JobView(ListView):
+    model = Job
+
+
+class JobRunView(ListView):
+    model = JobRun
